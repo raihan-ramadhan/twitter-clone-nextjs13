a4a5e5ccec5166abe1ef29c5e7d58d93c99f2380
raihan ramadhan <raihanramadhan.rn22@gmail.com>
2023-04-15 01:58:16 +0700

    return xs width to 500px

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      xs: "550px",
+      xs: "500px",
       ...defaultTheme.screens,
     },
     extend: {
